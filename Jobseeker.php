14994f359615905cf03bdd03b40d995339d5875a
2Echo <49248731+hanhao2006@users.noreply.github.com>
2020-01-02 03:11:08 -0500

    update

---
+++ Jobseeker.php
@@ -0,0 +1,135 @@
+<?php 
+
+class Jobseekders {
+    
+    // Properties
+
+    private $firstName;
+    private $lastName;
+    private $address;
+    private $email;
+    private $phoneNumber;
+    private $passworld;
+    /**
+     * Get the value of firstName
+     */ 
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @return  self
+     */ 
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     */ 
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @return  self
+     */ 
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of address
+     */ 
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set the value of address
+     *
+     * @return  self
+     */ 
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of phoneNumber
+     */ 
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * Set the value of phoneNumber
+     *
+     * @return  self
+     */ 
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of passworld
+     */ 
+    public function getPassworld()
+    {
+        return $this->passworld;
+    }
+
+    /**
+     * Set the value of passworld
+     *
+     * @return  self
+     */ 
+    public function setPassworld($passworld)
+    {
+        $this->passworld = $passworld;
+
+        return $this;
+    }
+
+    
+}
+?>
